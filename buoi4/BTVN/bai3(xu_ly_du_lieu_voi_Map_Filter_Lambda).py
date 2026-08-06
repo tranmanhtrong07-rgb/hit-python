c37c95a7da69860001c22ddb5a190f816d2176c6
tranmanhtrong07-rgb <tranmanhtrong07@gmail.com>
2026-08-06 22:30:13 +0700

    btvn

--- buoi4/BTVN/bai3(xu_ly_du_lieu_voi_Map_Filter_Lambda).py
+++ buoi4/BTVN/bai3(xu_ly_du_lieu_voi_Map_Filter_Lambda).py
@@ -22,9 +22,7 @@ sp_dien_tu = list(filter(lambda sp: sp['Danh mục'] == 'điện tử', danh_sac
 print('Sản phẩm điện tử: ', sp_dien_tu)
 sp_ban_het = list(filter(lambda sp: sp['Tồn kho'] == 0, danh_sach_sp))
 print('Sản phẩm đã bán hết: ', sp_ban_het)
-ds_ten_sp = []
-ten = map(lambda sp: sp['Tên sản phẩm'], danh_sach_sp)
-ds_ten_sp.append(ten)
+ds_ten_sp = list(map(lambda sp: sp['Tên sản phẩm'], danh_sach_sp))
 print('Các sản phẩm trong kho:', ds_ten_sp)
 sp_cao_cap = list(filter(lambda sp: sp["Giá"] >= 1000000, danh_sach_sp))
 thong_bao = list(map(print('Tặng voucher 100k cho khách mua', sp['Tên sản phẩm']), sp_cao_cap))
